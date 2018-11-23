8b3e81d4e22afb170a2a3ebddf9032d0a103715f
Adriano1lp <adrianolima1p@gmail.com>
2018-11-23 02:04:13 -0200

    Adicionando Projeto

---
+++ production/target/appoficial-0.0.1-SNAPSHOT/lib/js/controllers/infoCqCtrl.js
@@ -0,0 +1,71 @@
+angular.module("infoCq").controller("infoCqCtrl",function($scope, $http){
+	$scope.app = "Informações de C.Q";
+	$scope.materiais = [];
+	$scope.statuscqs = [];
+	
+	$scope.data = new Date();
+	$scope.username = localStorage.getItem("username");
+	
+	var carregarMaterial = function () {
+	    $http.get("http://localhost:8080/appoficial/cq").then(function (response) {
+	    	$scope.materiais = response.data;
+	    });
+	};
+	
+	var carregaHistorico = function () {
+	    $http.get("http://localhost:8080/appoficial/cqhistorico").then(function (response) {
+	    	$scope.materiais = response.data;
+	    });
+	};
+	
+	var carregarStatus = function () {
+	    $http.get('lib/json/defeitos.json').then(function (response) {
+	    	$scope.statuscqs = response.data;
+	    });
+	};
+	
+	$scope.adicionarCq = function(material){
+		material['login']=$scope.username;
+		console.log(material);
+		$http.post("http://localhost:8080/appoficial/cq", material).then(function (response) {
+	    	delete $scope.material.serie;
+	    	delete $scope.material.observacao;
+	   
+			$scope.materialForm.$setPristine();
+			carregarMaterial();
+	    });
+        
+	};
+	
+	$scope.pesquisaHistorico = function(material){
+		material['login']=$scope.username;
+		console.log(material);
+		$http.post("http://localhost:8080/appoficial/cqhistorico", material).then(function (response) {
+	    	delete $scope.material.serie;
+	    	delete $scope.material.observacao;
+	    	delete $scope.material.defeito;
+			$scope.materialForm.$setPristine();
+			carregaHistorico();
+	    });
+        
+	};
+	
+	$scope.deletarCq = function (materiais){
+		$scope.materiais = materiais.filter(function (material){			
+			if (!material.selecionado) return material;
+			material['login']=$scope.username;
+			$http.post("http://localhost:8080/appoficial/cqdelete", material).then(function (response) {		    	
+		    });			
+		});        
+    };
+    $scope.isMaterialSelecionado = function (materiais){
+       return materiais.some(function (material){
+            return material.selecionado;
+        }); 
+	};
+	$scope.ordenarPor = function (campo){
+        $scope.criterioDeOrdenacao = campo;
+        $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+    };
+	carregarMaterial();
+});
